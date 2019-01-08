67bd8a30f0ce6d652485f57e431a2d2435b010c8
qiandeyang <407571254@qq.com>
2019-01-08 18:02:54 +0800

    加上权限验证

--- app/Http/Services/ValidateCodeService.php
+++ app/Http/Services/ValidateCodeService.php
@@ -9,8 +9,10 @@ class ValidateCodeService
 {
     public static function checkValidate($request,$code)
     {
+        echo $code;
         $sessionCode = $request->session()->get('validateCode');
-
+echo "---------------------";
+        echo $sessionCode;
         $request->session()->put('validateCode','');
         if (strtolower($code) == $sessionCode) {
             return true;
