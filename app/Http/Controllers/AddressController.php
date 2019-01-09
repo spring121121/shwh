781e2273d41beca0ddc0fcabe337affe0a6837e0
qiandeyang <407571254@qq.com>
2019-01-09 15:52:54 +0800

    收货地址列表

--- app/Http/Controllers/AddressController.php
+++ app/Http/Controllers/AddressController.php
@@ -10,6 +10,8 @@
 namespace App\Http\Controllers;
 
 
+use App\Http\Services\AddressService;
+use App\Http\Services\UserService;
 use Illuminate\Http\Request;
 use App\models\AddressModel;
 use Validator;
@@ -21,14 +23,17 @@ class AddressController extends BaseController
      * @param Request $request
      * @return \Illuminate\Http\JsonResponse
      */
-    public function addressList(Request $request){
+    public function addressList(Request $request)
+    {
 
         $addressModel = new AddressModel();
-        $uid = $request->session()->get('userInfo')['id'];
-        $addressList = $addressModel::where('uid',$uid)
-            ->join('provinces','address.province','=','provinces.provinceid')
-            ->join('cities','address.city','=','cities.cityid')
-            ->join('areas','address.area','=','areas.areaid')
+        $uid = UserService::getUid($request);
+        $addressList = $addressModel::where('uid', $uid)
+            ->join('provinces as p', 'address.province', '=', 'p.provinceid')
+            ->join('cities as c', 'address.city', '=', 'c.cityid')
+            ->join('areas as a', 'address.area', '=', 'a.areaid')
+            ->select('address.*','p.province','c.city','a.area')
+
             ->get()->toArray();
 
         return $this->success($addressList);
@@ -40,9 +45,10 @@ class AddressController extends BaseController
      * @param Request $request
      * @return \Illuminate\Http\JsonResponse
      */
-    public function addAddress(Request $request){
+    public function addAddress(Request $request)
+    {
         $rules = [
-            'name' =>'required',
+            'name' => 'required',
             'province' => 'required',
             'city' => 'required',
             'area' => 'required',
@@ -50,17 +56,22 @@ class AddressController extends BaseController
             'mobile' => 'required'
         ];
 
-        $validator = Validator::make($request->all(),$rules);
-        if($validator->fails()){
-            return $this->fail(50001,$validator->errors()->all());
+        $validator = Validator::make($request->all(), $rules);
+        if ($validator->fails()) {
+            return $this->fail(50001, $validator->errors()->all());
         }
 
-        $uid = $request->session()->get('userInfo')['id'];
+        $uid = UserService::getUid($request);
         $addressInfo = $request->input('address_info');
         $province = $request->input('province');
         $city = $request->input('city');
-        $area= $request->input('area');
+        $area = $request->input('area');
         $mobile = $request->input('mobile');
+        $is_default = $request->input('is_default', 0);
+
+        //把其他收货地址都改为非默认
+        AddressService::updateNotDefault($uid);
+
         $addressModel = new AddressModel();
         $addressModel->uid = $uid;
         $addressModel->province = $province;
@@ -68,11 +79,12 @@ class AddressController extends BaseController
         $addressModel->area = $area;
         $addressModel->address_info = $addressInfo;
         $addressModel->mobile = $mobile;
+        $addressModel->is_default = $is_default;
         $re = $addressModel->save();
 
-        if($re){
+        if ($re) {
             return $this->success();
-        }else{
+        } else {
             return $this->fail(300);
         }
     }
@@ -82,10 +94,11 @@ class AddressController extends BaseController
      * @param Request $request
      * @return \Illuminate\Http\JsonResponse
      */
-    public function updateAddress(Request $request){
+    public function updateAddress(Request $request)
+    {
         $rules = [
-            'id'=>'required',
-            'name'=>'required',
+            'id' => 'required',
+            'name' => 'required',
             'province' => 'required',
             'city' => 'required',
             'area' => 'required',
@@ -93,61 +106,63 @@ class AddressController extends BaseController
             'mobile' => 'required'
         ];
 
-        $validator = Validator::make($request->all(),$rules);
+        $validator = Validator::make($request->all(), $rules);
 
-        if($validator->fails()){
-            return $this->fail(50001,$validator->errors()->all());
+        if ($validator->fails()) {
+            return $this->fail(50001, $validator->errors()->all());
         }
 
-        $uid = $request->session()->get('userInfo')['id'];
+        $uid = UserService::getUid($request);
 //        $uid = 1;
         $id = $request->input('id');
         $addressInfo = $request->input('address_info');
         $province = $request->input('province');
         $city = $request->input('city');
-        $area= $request->input('area');
+        $area = $request->input('area');
         $mobile = $request->input('mobile');
 
 
         $updateArr = [
-            'province'=>$province,
-            'city'=>$city,
-            'area'=>$area,
-            'address_info'=>$addressInfo,
-            'mobile'=>$mobile
-            ];
+            'province' => $province,
+            'city' => $city,
+            'area' => $area,
+            'address_info' => $addressInfo,
+            'mobile' => $mobile
+        ];
         $addressModel = new AddressModel();
 
-        $re = $addressModel::where('id','=',$id)
-            ->where('uid','=',$uid)
+        $re = $addressModel::where('id', '=', $id)
+            ->where('uid', '=', $uid)
             ->update($updateArr);
 
-        if($re){
+        if ($re) {
             return $this->success();
-        }else{
+        } else {
             return $this->fail(300);
         }
     }
 
     /**
      * 设置默认地址
+     * @param Request $request
      * @param $id
      * @return \Illuminate\Http\JsonResponse
      */
-    public function setDefaultAddress(Request $request,$id){
+    public function setDefaultAddress(Request $request, $id)
+    {
         //先检测该用户是否已经存在默认地址
         $addressModel = new AddressModel();
-        $uid = $request->session()->get('userInfo')['id'];
-        $update = ['is_default'=>AddressModel::IS_DEFAULT_0];
-        $addressModel::where('uid','=',$uid)->update($update);
+        $uid = UserService::getUid($request);
+        $update = ['is_default' => AddressModel::IS_DEFAULT_0];
+        $addressModel::where('uid', '=', $uid)->update($update);
 
 
-        $updateArr = ['is_default'=>AddressModel::IS_DEFAULT_1];
+        $updateArr = ['is_default' => AddressModel::IS_DEFAULT_1];
 
-        $re = $addressModel::where('id','=',$id)->update($updateArr);
-        if($re){
+        $re = $addressModel::where('id', '=', $id)->update($updateArr);
+        if ($re) {
             return $this->success();
-        }else{
+        } else {
             return $this->fail(300);
         }
     }
@@ -157,12 +172,13 @@ class AddressController extends BaseController
      * @param Request $request
      * @return \Illuminate\Http\JsonResponse
      */
-    public function addressDetail(Request $request){
+    public function addressDetail(Request $request)
+    {
         $id = $request->input('id');
-        $addressDeail = AddressModel::where('address.id',$id)
-            ->join('provinces','address.province','=','provinces.provinceid')
-            ->join('cities','address.city','=','cities.cityid')
-            ->join('areas','address.area','=','areas.areaid')
+        $addressDeail = AddressModel::where('address.id', $id)
+            ->join('provinces', 'address.province', '=', 'provinces.provinceid')
+            ->join('cities', 'address.city', '=', 'cities.cityid')
+            ->join('areas', 'address.area', '=', 'areas.areaid')
             ->get()->toArray();
         return $this->success($addressDeail);
     }
