99e2118687eb3e67a3e73bf740f9d7593fc68a5a
qiandeyang <407571254@qq.com>
2019-01-11 17:38:27 +0800

    获取我收藏的笔记

---
+++ app/Http/Controllers/LikeController.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: shiminmin
+ * Date: 18-12-23
+ * Time: 下午4:30
+ */
+
+
+namespace App\Http\Controllers;
+
+
+use App\models\LikesModel;
+use Illuminate\Http\Request;
+use App\Http\Services\UserService;
+use App\Http\Services\ForwardService;
+use App\Http\Services\LikesService;
+use App\Http\Services\CommentService;
+
+class LikeController extends BaseController
+{
+    public function getMyLikeNote(Request $request)
+    {
+        $uid = UserService::getUid($request);
+        $likeModel = new LikesModel();
+        $limit = $request->get('limit', 10);
+        $page = $request->get('page', 1);
+        $offset = ($page - 1) * $limit;
+        $collectNote = $likeModel::where('likes.uid', '=', $uid)
+            ->join('note', 'note.id', '=', 'likes.note_id')
+            ->skip($offset)->take($limit)->get();
+        foreach ($collectNote as $note) {
+            $note->forwardNum = ForwardService::getForwardNum($note->note_id);
+            $note->likeNum = LikesService::getLikesNum($note->note_id);
+            $note->commentNum = CommentService::getCommentNum($note->note_id);
+        }
+
+        if ($collectNote) {
+            return $this->success($collectNote);
+        } else {
+            return $this->fail(300);
+        }
+
+    }
+
+
+}
