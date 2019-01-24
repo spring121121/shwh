d1dbaf20c8389b53e3e27eee901a258065433e9a
glj <grj@bwgsd.com>
2019-01-25 01:04:08 +0800

    新增店铺对接接口

--- resources/views/personal/register-store.blade.php
+++ resources/views/personal/register-store.blade.php
@@ -32,12 +32,12 @@
                     <div class="ipt-logo-box" id="store-logo-box"><input type="file" class="ipt-file" id="store-logo" name="source"></div>
                 </div>
                 <div class="store-cont">
-                    <label for="store-name">真实姓名</label>
-                    <div class="ipt-name-box"><input type="text" maxlength="16" id="store-name" placeholder="请填写店主的真实姓名"></div>
+                    <label for="store-id-name">真实姓名</label>
+                    <div class="ipt-name-box"><input type="text" maxlength="16" id="store-id-name" placeholder="请填写店主的真实姓名"></div>
                 </div>
                 <div class="store-cont">
-                    <label for="store-name">身份证号</label>
-                    <div class="ipt-name-box"><input type="text" maxlength="16" id="store-name" placeholder="请填写店主的身份证号码"></div>
+                    <label for="store-id-card">身份证号</label>
+                    <div class="ipt-name-box"><input type="text" maxlength="18" id="store-id-card" placeholder="请填写店主的身份证号码"></div>
                 </div>
                 <div class="store-cont store-prove">
                     <label>身份证正反面</label>
@@ -134,45 +134,60 @@
                 //         });
                 //     }
                 // });
-            }else {
-                $(".btn-store-register").click(function () {
-                    var store_name = $("#store-name").val(),
-                        store_brief = $("#store-brief").val(),
-                        store_logo = $("#store-logo-box").find("img").attr("src"),
-                        store_prove = $("#store-prove-box").find("img").attr("src");
-                    console.log(store_name,store_brief,store_logo,store_prove)
-                    if (store_name == ""){
-                        alert("店铺名不能为空")
-                    }else if (store_brief == ""){
-                        alert("请简单介绍一下自己的店铺吧")
-                    }else if (store_logo == undefined) {
-                        alert("请上传店铺logo")
-                    }else if (store_prove == undefined){
-                        alert("请上传营业执照")
-                    }else {
-                        $.ajax({
-                            url : "/addStore",	//请求url
-                            type : "post",	//请求类型  post|get
-                            dataType : "json",  //返回数据的 类型 text|json|html--
-                            data: {
-                                'store[name]':store_name,
-                                'store[introduction]':store_brief,
-                                'store[logo_pic_url]':store_logo,
-                                'store[prove_url]':store_prove
-                            },
-                            success : function(data){//回调函数 和 后台返回的 数据
-                                console.log(data)
-                                if (data.status){
-                                    alert("信息提交成功，请静心等待3-5个工作日");
-                                    window.location.href = "/wap/personal";
-                                }else {
-                                    alert("提交失败，请重试");
-                                }
-                            }
-                        });
-                    }
-                });
             }
+            $(".btn-store-register").click(function () {
+                var store_name = $("#store-name").val(),
+                    store_brief = $("#store-brief").val(),
+                    store_logo = $("#store-logo-box").find("img").attr("src"),
+                    store_id_name = $("#store-id-name").val(),
+                    store_id_card = $("#store-id-card").val(),
+                    store_id_just = $("#sfz-just-box").find("img").attr("src"),
+                    store_id_back = $("#sfz-back-box").find("img").attr("src"),
+                    store_prove = $("#store-prove-box").find("img").attr("src");
+                if (store_name == ""){
+                    alert("店铺名不能为空")
+                }else if (store_brief == ""){
+                    alert("请简单介绍一下自己的店铺吧")
+                }else if ($("#store-logo-box").find("img").length == 0) {
+                    alert("请上传店铺logo")
+                }else if (store_id_name == "") {
+                    alert("请输入您的真实姓名")
+                }else if (store_id_card == "") {
+                    alert("请输入您的身份证号")
+                }else if ($("#sfz-just-box").find("img").length == 0) {
+                    alert("请上传您的身份证正面")
+                }else if ($("#sfz-back-box").find("img").length == 0) {
+                    alert("请上传您的身份证反面")
+                }else if ($("#store-prove-box").find("img").length == 0 && role_id != 3){
+                    alert("请上传营业执照")
+                }else {
+                    console.log(store_name,store_brief,store_logo,store_id_name,store_id_card,store_id_just,store_id_back,store_prove)
+                    $.ajax({
+                        url : "/addStore",	//请求url
+                        type : "post",	//请求类型  post|get
+                        dataType : "json",  //返回数据的 类型 text|json|html--
+                        data: {
+                            'store[name]':store_name,
+                            'store[introduction]':store_brief,
+                            'store[logo_pic_url]':store_logo,
+                            'store[real_name]':store_id_name,
+                            'store[id_card_num]':store_id_card,
+                            'store[id_card_front]':store_id_just,
+                            'store[id_card_backend]':store_id_back,
+                            'store[role_id]':role_id,
+                            'store[prove_url]':store_prove
+                        },
+                        success : function(data){//回调函数 和 后台返回的 数据
+                            if (data.status){
+                                alert("信息提交成功，请静心等待3-5个工作日");
+                                window.location.href = "/wap/personal";
+                            }else {
+                                alert(data.message);
+                            }
+                        }
+                    });
+                }
+            });
         });
         function store_upload(id) {
             $.ajaxFileUpload({
