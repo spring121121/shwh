e09d94bd746021fcc8eab46a52569e1d92638831
JHR <jianghairui@sina.cn>
2019-01-25 14:11:46 +0800

    20190125

--- app/Http/Controllers/WxpayController.php
+++ app/Http/Controllers/WxpayController.php
@@ -13,20 +13,6 @@ use Illuminate\Support\Facades\DB;
 
 class WxpayController extends BaseController {
 
-    public $config = [];
-
-    public function __construct()
-    {
-        $this->config = [
-            'appid' => 'wx1dc64acc9bd9eb09',
-            'app_secret' => '18030345ebbbc089f628a5eb1db5cda3',
-            'mch_id' => '1490402642',
-            'appkey' => 'TIANJINTAOCIYUAN20190111SHWHCOPY',
-            'sslcert_path' => '/var/www/html/public/cert/apiclient_cert.pem',
-            'sslkey_path' => '/var/www/html/public/cert/apiclient_key.pem'
-        ];
-    }
-
     public function pay(Request $request) {
         $appid = $this->config['appid'];
         $secret = $this->config['app_secret'];
@@ -95,7 +81,7 @@ class WxpayController extends BaseController {
             /*--------------微信统一下单--------------*/
             if ($result['return_code'] == 'SUCCESS' && $result['result_code'] == 'SUCCESS') {
                 try {
-                    OrdersModel::where('pay_order_sn',$pay_order_sn)->update(['pay_order_sn'=>$pay_order_sn,'openid'=>$openid,'prepay_time'=>time()]);
+                    OrdersModel::where('pay_order_sn',$pay_order_sn)->update(['openid'=>$openid,'prepay_time'=>time()]);
                 }catch (\Exception $e) {
                     exit('<script>alert("'.$e->getMessage().'");document.addEventListener("WeixinJSBridgeReady", function(){ WeixinJSBridge.call("closeWindow"); }, false);</script>');
                 }
