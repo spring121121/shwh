9e26065e98c91b028d16758ebc65e0453385787e
glj <grj@bwgsd.com>
2019-01-28 17:58:10 +0800

    甘荣健

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -54,9 +54,9 @@
         <div id="bander" onclick="toActive()">
             <div class="swiper-container swiper-add">
                 <div class="swiper-wrapper">
-                    <div class="swiper-slide"><img src="/images/banner1.jpg" alt=""></div>
-                    <div class="swiper-slide"><img src="/images/banner2.jpg" alt=""></div>
-                    <div class="swiper-slide"><img src="/images/banner3.jpg" alt=""></div>
+                    <div class="swiper-slide" style="overflow: hidden"><img src="/images/banner1.jpg" alt=""></div>
+                    <div class="swiper-slide" style="overflow: hidden"><img src="/images/banner2.jpg" alt=""></div>
+                    <div class="swiper-slide" style="overflow: hidden"><img src="/images/004.jpg" alt=""></div>
                 </div>
                 <!-- Add Pagination -->
                 <div class="swiper-pagination"></div>
@@ -96,25 +96,25 @@
         <!--文创故事-->
         <div class="culture">
         	<p>文创故事</p>
-                <video src="/images/viode.mp4" controls="controls" width="100%" height="100%" >
+                <video id="video" src="/images/viode.mp4" loop controls="controls" width="100%" height="100%" >
                     your browser does not support the video tag
                 </video>
             </div>
         <div class="culture">
             <p>文创故事</p>
-            <video src="/images/viode.mp4" controls="controls" width="100%" height="100%" >
+            <video src="/images/viode.mp4" loop controls="controls" width="100%" height="100%" >
                 your browser does not support the video tag
             </video>
         </div>
         <div class="culture">
             <p>文创故事</p>
-            <video src="/images/viode.mp4" controls="controls" width="100%" height="100%" >
+            <video src="/images/viode.mp4" loop controls="controls" width="100%" height="100%" >
                 your browser does not support the video tag
             </video>
         </div>
         <div class="culture">
             <p>文创故事</p>
-            <video src="/images/viode.mp4" controls="controls" width="100%" height="100%" >
+            <video src="/images/viode.mp4" loop controls="controls" width="100%" height="100%" >
                 your browser does not support the video tag
             </video>
         </div>
